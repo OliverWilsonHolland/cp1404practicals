dbf6b8dc4d71254551fd9ac4f432bd24dfb72f23
Oliver Wilson Holland <oliver.wilsonholland@my.jcu.edu.au>
2020-09-03 21:25:19 +1000

    finalise list exercises

---
+++ Prac_04/list_exercises.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 Practical
+Data file -> List exercises
+"""
+
+# numbers = []
+# for i in range(0, 5):
+#     number = int(input("Number: "))
+#     numbers.append(number)
+# print("The first number is", numbers[0])
+# print("The last number is", numbers[-1])
+# print("The smallest number is ", min(numbers))
+# print("The largest number is", max(numbers))
+# print("The average of the numbers is", sum(numbers) / 5)
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+             'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = str(input("Username: "))
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
