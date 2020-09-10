99a2d97b30ec85ec36882931e590999f1a6f3825
Oliver Wilson Holland <oliver.wilsonholland@my.jcu.edu.au>
2020-09-10 23:02:45 +1000

    add hex colour program

--- Prac_05/hex_colours.py
+++ Prac_05/hex_colours.py
@@ -1,3 +1,7 @@
+"""
+CP1404/CP5632 Practical
+Hex colours in a dictionary
+"""
 HEX_COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
                 "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4",
                "aquamarine2": "#76eec6", "aquamarine4": "#458b74", "azure1": "#f0ffff"}
