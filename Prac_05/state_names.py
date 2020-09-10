2dadea105d1095f41d78a53ff39b1e57b0af633c
Oliver Wilson Holland <oliver.wilsonholland@my.jcu.edu.au>
2020-09-10 22:41:12 +1000

    add state names program

--- Prac_05/state_names.py
+++ Prac_05/state_names.py
@@ -5,16 +5,16 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 
 # state_code = input("Enter short state: ").upper()
 # while state_code != "":
-#     if state_code in CODE_TO_NAME:
-#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     if state_code in STATE_NAMES:
+#         print(state_code, "is", STATE_NAMES[state_code])
 #     else:
 #         print("Invalid short state")
 #     state_code = input("Enter short state: ").upper()
 
-for key, value in CODE_TO_NAME.items():
+for key, value in STATE_NAMES.items():
     print("{:3} is {}".format(key, value))
